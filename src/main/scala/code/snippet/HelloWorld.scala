93b8395f1ae79e7f18d9c8030974a342056adab6
abdhesh <abdhesh@knoldus.com>
2014-11-05 21:53:19 +0530

    Test localization

--- src/main/scala/code/snippet/HelloWorld.scala
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -1,18 +1,26 @@
-package code 
-package snippet 
+package code
+package snippet
 
-import scala.xml.{NodeSeq, Text}
+import scala.xml.{ NodeSeq, Text }
 import net.liftweb.util._
 import net.liftweb.common._
 import java.util.Date
 import code.lib._
 import Helpers._
+import net.liftweb.http.S
+import java.util.Locale
 
-class HelloWorld {
+class HelloWorld extends Loggable {
   lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
 
   // replace the contents of the element with id "time" with the date
-  def howdy = "#time *" #> date.map(_.toString)
+  def howdy = {
+
+    logger.info("::::English:::::" + S.?("invalid.email.address", new Locale("en")))
+    logger.info("::::German:::::" + S.?("invalid.email.address", new Locale("de")))
+    logger.info(":::Default:::::" + S.?("invalid.email.address"))
+    "#time *" #> date.map(_.toString)
+  }
 
   /*
    lazy val date: Date = DependencyFactory.time.vend // create the date via factory
